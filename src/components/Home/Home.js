f936260825d0f3d8f18d7ee44ea3f7f605cae47c
github username <github email address>
2019-06-08 18:18:33 -0400

    forgot to push things up on EOD Friday. looks like background image stuff.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div id='main-container' className='ui container'>
+        <div style={{ backgroundImage: 'url(./home_background.jpg)', backgroundSize: `cover`, backgroundRepeat: `no-repeat`, backgroundPosition: `center`, width: `100vw`, height: `100vh`, overflow: `hidden`}}>
             <h2 className='ui header'>Home</h2>
-            <img src='fitness_wallpaper.jpg' alt='oops'></img>
+            {/* <img src='fitness_wallpaper.jpg' alt='oops'></img> */}
         </div>
     )
 }
--- src/containers/Main/MainContainer.js
+++ src/containers/Main/MainContainer.js
@@ -3,9 +3,9 @@ import React from 'react';
 
 const MainContainer = () => {
     return (
-        <div id='main-container' className='ui container'>
+        <div style={{ backgroundImage: 'home_background.jpeg', backgroundSize: `cover`, backgroundRepeat: `no-repeat`, backgroundPosition: `center`, width: `100%`, height: `100%`, overflow: `hidden`}}>
             <h2 className='ui header'>Main</h2>
-            <img src='fitness_wallpaper.jpg' alt='oops'></img>
+            {/* <img src='fitness_wallpaper.jpg' alt='oops'></img> */}
         </div>
     )
 }
