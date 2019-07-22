fc5432a78a9fd4e137ef510f09d272a478e87f5e
github username <github email address>
2019-07-22 17:19:22 +0900

    user settings, calories, and macros are updating on user setting change submit

--- src/MainContainer.js
+++ src/MainContainer.js
@@ -9,6 +9,100 @@ import { Segment, Button, Modal } from 'semantic-ui-react';
 import UserSetting from './UserSetting';
 import UserStat from './UserStat';
 
+export function getMacroData(data) {
+    let pCap = data.set_protein
+    let pTotal = 0
+    let pConsume = 0
+    let pRemain = 0
+    let pExceed = 0
+
+    let cCap = data.set_carbs
+    let cTotal = 0
+    let cConsume = 0
+    let cRemain = 0
+    let cExceed = 0
+
+    let fCap = data.set_fat
+    let fTotal = 0
+    let fConsume = 0
+    let fRemain = 0
+    let fExceed = 0
+
+    data.meals.map(meal => {
+        let p = meal.protein
+        let c = meal.carbs
+        let f = meal.fat
+
+        pTotal += p
+        cTotal += c
+        fTotal += f
+    })
+
+    if (pTotal >= pCap) {
+        pRemain = 0
+        pConsume = pCap
+        pExceed = pTotal - pCap
+    } else {
+        pConsume = pTotal
+        pRemain = pCap - pTotal
+        pExceed = 0
+    };
+
+
+    if (cTotal >= cCap) {
+        cRemain = 0
+        cConsume = cCap
+        cExceed = cTotal - cCap
+    } else {
+        cConsume = cTotal
+        cRemain = cCap - cTotal
+        cExceed = 0
+    };
+
+
+    if (fTotal >= fCap) {
+        fRemain = 0
+        fConsume = fCap
+        fExceed = fTotal - fCap
+    } else {
+        fConsume = fTotal
+        fRemain = fCap - fTotal
+        fExceed = 0
+    };
+
+    let seriesData = [{name: "Consumed (g)", data: [ pConsume, cConsume, fConsume ] },{name: "Remaining (g)", data: [ pRemain, cRemain, fRemain ] },{name: "Exceeded (g)", data: [ pExceed, cExceed, fExceed ] }]
+    return seriesData
+
+}
+
+export function getCaloriesData(data) {
+    let target = data.set_calories
+    let currentCount = 0
+    let exceeded = 0
+    let currentPercent = 0
+    let exceedPercent = 0
+
+    data.meals.map(meal => {
+        currentCount += meal.calories
+    })
+
+    if (target === null || target === 0) {
+        currentPercent = 0
+        exceedPercent = 0
+    } else if (currentCount > target) {
+        exceeded = currentCount - target
+        currentPercent = 100
+        exceedPercent = Math.round(exceeded/target * 100)
+    } else {
+        currentPercent = Math.round(currentCount/target * 100)
+        exceedPercent = 0
+    };
+
+    let seriesData = [currentPercent, exceedPercent]
+
+    return seriesData
+}
+
 class MainContainer extends React.Component {
 
     constructor() {
@@ -25,101 +119,100 @@ class MainContainer extends React.Component {
         }
         return true;
     }
-    
 
-    getMacroData(data) {
-            let pCap = data.set_protein
-            let pTotal = 0
-            let pConsume = 0
-            let pRemain = 0
-            let pExceed = 0
+    // export function getMacroData(data) {
+    //         let pCap = data.set_protein
+    //         let pTotal = 0
+    //         let pConsume = 0
+    //         let pRemain = 0
+    //         let pExceed = 0
     
-            let cCap = data.set_carbs
-            let cTotal = 0
-            let cConsume = 0
-            let cRemain = 0
-            let cExceed = 0
+    //         let cCap = data.set_carbs
+    //         let cTotal = 0
+    //         let cConsume = 0
+    //         let cRemain = 0
+    //         let cExceed = 0
     
-            let fCap = data.set_fat
-            let fTotal = 0
-            let fConsume = 0
-            let fRemain = 0
-            let fExceed = 0
+    //         let fCap = data.set_fat
+    //         let fTotal = 0
+    //         let fConsume = 0
+    //         let fRemain = 0
+    //         let fExceed = 0
     
-            data.meals.map(meal => {
-                let p = meal.protein
-                let c = meal.carbs
-                let f = meal.fat
+    //         data.meals.map(meal => {
+    //             let p = meal.protein
+    //             let c = meal.carbs
+    //             let f = meal.fat
     
-                pTotal += p
-                cTotal += c
-                fTotal += f
-            })
+    //             pTotal += p
+    //             cTotal += c
+    //             fTotal += f
+    //         })
     
-            if (pTotal >= pCap) {
-                pRemain = 0
-                pConsume = pCap
-                pExceed = pTotal - pCap
-            } else {
-                pConsume = pTotal
-                pRemain = pCap - pTotal
-                pExceed = 0
-            };
-    
-    
-            if (cTotal >= cCap) {
-                cRemain = 0
-                cConsume = cCap
-                cExceed = cTotal - cCap
-            } else {
-                cConsume = cTotal
-                cRemain = cCap - cTotal
-                cExceed = 0
-            };
+    //         if (pTotal >= pCap) {
+    //             pRemain = 0
+    //             pConsume = pCap
+    //             pExceed = pTotal - pCap
+    //         } else {
+    //             pConsume = pTotal
+    //             pRemain = pCap - pTotal
+    //             pExceed = 0
+    //         };
     
     
-            if (fTotal >= fCap) {
-                fRemain = 0
-                fConsume = fCap
-                fExceed = fTotal - fCap
-            } else {
-                fConsume = fTotal
-                fRemain = fCap - fTotal
-                fExceed = 0
-            };
+    //         if (cTotal >= cCap) {
+    //             cRemain = 0
+    //             cConsume = cCap
+    //             cExceed = cTotal - cCap
+    //         } else {
+    //             cConsume = cTotal
+    //             cRemain = cCap - cTotal
+    //             cExceed = 0
+    //         };
     
-            let seriesData = [{name: "Consumed (g)", data: [ pConsume, cConsume, fConsume ] },{name: "Remaining (g)", data: [ pRemain, cRemain, fRemain ] },{name: "Exceeded (g)", data: [ pExceed, cExceed, fExceed ] }]
-            return seriesData
+    
+    //         if (fTotal >= fCap) {
+    //             fRemain = 0
+    //             fConsume = fCap
+    //             fExceed = fTotal - fCap
+    //         } else {
+    //             fConsume = fTotal
+    //             fRemain = fCap - fTotal
+    //             fExceed = 0
+    //         };
+    
+    //         let seriesData = [{name: "Consumed (g)", data: [ pConsume, cConsume, fConsume ] },{name: "Remaining (g)", data: [ pRemain, cRemain, fRemain ] },{name: "Exceeded (g)", data: [ pExceed, cExceed, fExceed ] }]
+    //         return seriesData
 
-    }
+    // }
 
-    getCaloriesData(data) {
-            let target = data.set_calories
-            let currentCount = 0
-            let exceeded = 0
-            let currentPercent = 0
-            let exceedPercent = 0
+    // getCaloriesData(data) {
+    //         let target = data.set_calories
+    //         let currentCount = 0
+    //         let exceeded = 0
+    //         let currentPercent = 0
+    //         let exceedPercent = 0
     
-            data.meals.map(meal => {
-                currentCount += meal.calories
-            })
+    //         data.meals.map(meal => {
+    //             currentCount += meal.calories
+    //         })
     
-            if (target === null || target === 0) {
-                currentPercent = 0
-                exceedPercent = 0
-            } else if (currentCount > target) {
-                exceeded = currentCount - target
-                currentPercent = 100
-                exceedPercent = Math.round(exceeded/target * 100)
-            } else {
-                currentPercent = Math.round(currentCount/target * 100)
-                exceedPercent = 0
-            };
+    //         if (target === null || target === 0) {
+    //             currentPercent = 0
+    //             exceedPercent = 0
+    //         } else if (currentCount > target) {
+    //             exceeded = currentCount - target
+    //             currentPercent = 100
+    //             exceedPercent = Math.round(exceeded/target * 100)
+    //         } else {
+    //             currentPercent = Math.round(currentCount/target * 100)
+    //             exceedPercent = 0
+    //         };
     
-            let seriesData = [currentPercent, exceedPercent]
+    //         let seriesData = [currentPercent, exceedPercent]
     
-            return seriesData
-    }
+    //         return seriesData
+    // }
 
     userSettingOpen = () => this.props.dispatch({ type: "USER_SETTING_MODAL" })
 
@@ -131,8 +224,8 @@ class MainContainer extends React.Component {
         getUserData(userId).then((data) => {
             this.props.dispatch({ type: "CLEAR_USER_DATA", data: null })
             this.props.dispatch({ type: "GET_USER_DATA", data: data })
-            this.props.dispatch({ type: "GET_MACRO_DATA", data: this.getMacroData(data) })
-            this.props.dispatch({ type: "GET_CALORIES_DATA", data: this.getCaloriesData(data) })
+            this.props.dispatch({ type: "GET_MACRO_DATA", data: getMacroData(data) })
+            this.props.dispatch({ type: "GET_CALORIES_DATA", data: getCaloriesData(data) })
             }
         )
     }
