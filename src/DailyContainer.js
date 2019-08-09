e16ac2451e8855e236fc224ab4e0ab5d9cd73a93
github username <github email address>
2019-08-08 20:03:04 -0400

    pulled calories and macros data on Daily page

--- src/DailyContainer.js
+++ src/DailyContainer.js
@@ -1,16 +1,43 @@
 import React from 'react';
 import DailyProgress from './DailyProgress';
 import { Segment, Grid, Container } from 'semantic-ui-react';
+import { getUserData } from './ApiCalls';
+import jwt_decode from 'jwt-decode';
 import './css/DailyContainer.css';
+import { connect } from 'react-redux';
+import { calcMacroData, calcCaloriesData, formatDate } from './Calculations';
 
-const DailyContainer = () => {
-    return (
-        <Grid id='dailycontainer'>
-            <Segment>
-                <DailyProgress />
-            </Segment>
-        </Grid>
-    )
+class DailyContainer extends React.Component {
+
+
+    componentDidMount() {
+        const userId = jwt_decode(localStorage.getItem('token')).user_id
+        getUserData(userId).then((data) => {
+            let dateNow = new Date(this.props.currentDate)
+            data.meals = data.meals.filter(meal => new Date(formatDate(meal.date)) >= dateNow )
+            this.props.dispatch({ type: "CLEAR_USER_DATA", data: null })
+            this.props.dispatch({ type: "GET_USER_DATA", data: data })
+            this.props.dispatch({ type: "GET_MACRO_DATA", data: calcMacroData(data) })
+            this.props.dispatch({ type: "GET_CALORIES_DATA", data: calcCaloriesData(data) })
+            }
+        )
+    }
+
+    render() {
+        return (
+            <Grid id='dailycontainer'>
+                <Segment>
+                    <DailyProgress />
+                </Segment>
+            </Grid>
+        )
+    }
 }
 
-export default DailyContainer;
+let mapStateToProps = (state) => {
+    return {
+        currentDate: state.session_reducer.currentDate
+    }
+}
+
+export default connect(mapStateToProps)(DailyContainer);
